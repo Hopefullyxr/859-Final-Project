50805f5ebb9cc36542faa214222778968fd2e045
Grace <64219828+graceful-one@users.noreply.github.com>
2024-11-20 15:58:43 -0500

    Update one_dolphin_query.py

--- one_dolphin_query.py
+++ one_dolphin_query.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.page_link("your_app.py", label="Dolphin Query")
 st.page_link("photo_query.py", label="Picture app")
-st.page_link("pages/page_2.py", label="update data")
+#st.page_link("pages/page_2.py", label="update data")
 
 #import geopandas as gpd 
 st.title("Home Page")
